6e8e58c64d03805e28e1919ece66f345c8eb03ff
Marian Kica <77883550+CapitanMikon@users.noreply.github.com>
2024-11-13 18:33:02 +0100

    zaklad pohyb hraca

---
+++ Assets/MyProject/Scripts/ExplosionMaker.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExplosionMaker : MonoBehaviour
+{
+    public float explosionForce = 10f;
+    public float explosionRadius;
+    private Collider[] colliders = new Collider[10];
+    public LayerMask layerMask;
+    public Camera cam;
+    
+    private Vector3 click;
+    
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            var ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity))
+            {
+                click = hit.point;
+                int numColliders = Physics.OverlapSphereNonAlloc(hit.point, explosionRadius, colliders, layerMask);
+                Debug.Log(numColliders);
+
+                if (numColliders > 0)
+                {
+                    for (int i = 0; i < numColliders; i++)
+                    {
+                        if (colliders[i].attachedRigidbody)
+                        {
+                            colliders[i].attachedRigidbody.AddExplosionForce(explosionForce, hit.point, explosionRadius);
+                            Debug.Log(colliders[i].gameObject);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                click = Vector3.zero;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawWireSphere(click, explosionRadius);
+    }
+}
--- Assets/MyProject/Scripts/PlayerController.cs
+++ Assets/MyProject/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionReference movement;
     [SerializeField] private InputActionReference jump;
-    [SerializeField] private float speed = 30f;
+    [FormerlySerializedAs("speed")] [SerializeField] private float moveSpeed = 30f;
     [SerializeField] private float jumpForce = 3f;
     [SerializeField] private float maxSpeed = 30f;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform legs;
 
     [SerializeField] private bool grounded = true;
+
+    [SerializeField] private Vector2 input;
 
     private void Start()
     {
@@ -21,6 +24,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnEnable()
     {
+        movement.action.performed += OnMoveStart;
+        movement.action.canceled += OnMoveStart;
+    }
+    
+    private void OnDisable()
+    {
+        movement.action.performed -= OnMoveStart;
+        movement.action.canceled -= OnMoveStart;
     }
 
     private void Update()
@@ -40,39 +51,68 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        var dir = movement.action.ReadValue<Vector2>();
-        var newPos = new Vector3(dir.x, 0f, dir.y) * speed * Time.fixedDeltaTime;
+        CheckGrounded();
+        HandleMovement();
+    }
 
-        if (dir != Vector2.zero && grounded)
+    private void HandleMovement()
+    {
+        /*if (input == Vector2.zero)
         {
-            Vector3 velocity = newPos * speed * Time.fixedDeltaTime;
-            velocity.y = rb.velocity.y;
-            rb.velocity = velocity;
-        }
+            return;
+        }*/
+        input.Normalize();
+        
+        var currentVelocity = rb.velocity;
+        
+        var targetVelocity = new Vector3(input.x,0f, input.y) * moveSpeed;
+        // targetVelocity.x += currentVelocity.x;
+        // targetVelocity.z += currentVelocity.z;
+        // Vector3.ClampMagnitude(targetVelocity, maxSpeed);
+        // targetVelocity.y = currentVelocity.y;
 
-        if (Physics.Raycast(legs.transform.position, Vector3.down, out var hit, 0.2f))
-        {
-            Debug.Log(hit.collider.gameObject);
-            grounded = true;
-        }
-        else
-        {
-            grounded = false;
-        }
+        var changeVelocity = targetVelocity - currentVelocity;
+        changeVelocity = new Vector3(changeVelocity.x, 0f, changeVelocity.z);
+
+        Vector3.ClampMagnitude(changeVelocity, maxSpeed);
+        
+        rb.AddForce(changeVelocity, ForceMode.VelocityChange);
+
+        //rb.velocity = targetVelocity;
+
+        //currentVelocity = rb.velocity;
+        //Vector3.ClampMagnitude(currentVelocity, maxSpeed);
+        //currentVelocity.y = rb.velocity.y;
+    }
+
+    private void CheckGrounded()
+    {
+        // if (Physics.Raycast(legs.transform.position, Vector3.down, out var hit, 0.2f))
+        // {
+        //     //Debug.Log(hit.collider.gameObject);
+        //     grounded = true;
+        // }
+        // else
+        // {
+        //     grounded = false;
+        // }
+        // Debug.Log(hit.collider.gameObject);
+        grounded = Physics.Raycast(legs.transform.position, Vector3.down, out var hit, 0.2f);
     }
 
     private void OnMoveStart(InputAction.CallbackContext ctx)
     {
-        
+        input = ctx.ReadValue<Vector2>();
     }
 
     private void OnMoveEnd(InputAction.CallbackContext ctx)
     {
+        input = Vector2.zero;
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(legs.transform.position, Vector3.down * 0.2f);
-    }
+    // private void OnDrawGizmos()
+    // {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawRay(legs.transform.position, Vector3.down * 0.2f);
+    // }
 }
